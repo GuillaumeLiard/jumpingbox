2373444d3824dbbc1999546f734f09974928cf16
GuillaumeLiard <liardg@gmail.com>
2016-07-25 17:11:54 +0200

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var start = $("#start");
 var offset = start.offset();
 var rotation = 0;
-console.log(offset);
 
 $('.segment').click(function(){
     var segmentOffset = $(this).offset();
@@ -9,8 +8,4 @@ $('.segment').click(function(){
     rotation +=360;
     TweenMax.to('#box',1,{ease: Back.easeOut.config(6),top:deltaOffset});
     TweenMax.to('#box',1,{color:'blue',rotation:rotation});
-    // console.log();
 });
-
-
-console.log('tween started');
