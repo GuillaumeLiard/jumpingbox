f56fdab237377976dcd38a30f8523317e7b5136a
GuillaumeLiard <liardg@gmail.com>
2016-07-25 17:03:59 +0200

    jump with more style

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #jumpingbox
 
 Proof of concept for jumping between divs
+
+click on a div to make it jump !
+
+Jump relative to top of the div / top of the head
--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 var start = $("#start");
 var offset = start.offset();
+var rotation = 0;
 console.log(offset);
 
 $('.segment').click(function(){
     var segmentOffset = $(this).offset();
     var deltaOffset = segmentOffset.top - offset.top ;
-    TweenMax.to('#box',1,{color:'blue',top:deltaOffset,rotation:360});
+    rotation +=360;
+    TweenMax.to('#box',1,{ease: Back.easeOut.config(6),top:deltaOffset});
+    TweenMax.to('#box',1,{color:'blue',rotation:rotation});
     // console.log();
 });
 
