350b80ce01845275a8d6144dab76e0bbc9405eb4
GuillaumeLiard <liardg@gmail.com>
2016-07-26 00:39:07 +0200

    jump on top of div

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var start = $("#start");
 var offset = start.offset();
+var hChar = $('#box').height();
+console.log(hChar);
 var rotation = 0;
 
 $('.segment').click(function(){
     var segmentOffset = $(this).offset();
-    var deltaOffset = segmentOffset.top - offset.top ;
+    var deltaOffset = segmentOffset.top - offset.top - hChar;
     rotation +=360;
     TweenMax.to('#box',1,{ease: Back.easeOut.config(6),top:deltaOffset});
     TweenMax.to('#box',1,{color:'blue',rotation:rotation});
